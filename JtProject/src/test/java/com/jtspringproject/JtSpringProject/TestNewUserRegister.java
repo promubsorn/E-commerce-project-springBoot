35dd498be4f2682aaa80d7818ccb124f963e68a8
Kittipat Arpanon <kittipat.arp@student.mahidol.ac.th>
2023-10-08 23:25:46 +0700

    add 2 new test case include the register and getuser

---
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/TestNewUserRegister.java
@@ -0,0 +1,58 @@
+package com.jtspringproject.JtSpringProject;
+
+import com.jtspringproject.JtSpringProject.models.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.jtspringproject.JtSpringProject.controller.UserController;
+import org.apache.jasper.tagplugins.jstl.core.When;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.jtspringproject.JtSpringProject.services.userService;
+
+
+@SpringBootTest
+public class TestNewUserRegister {
+    @InjectMocks
+    private UserController userController; // Assuming the controller's name is UserController
+
+    @Mock
+    private userService userService; // Assuming the service's name is UserService
+
+    @Captor
+    private ArgumentCaptor<User> userCaptor;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testNewUserRegister1() {
+        //Given Valid register new user
+        User ValidUser = new User();
+        ValidUser.setUsername("TestUser");
+        ValidUser.setAddress("TestAddress");
+        ValidUser.setEmail("Test@example.com");
+        ValidUser.setPassword("password123");
+
+        // When
+        String result1 = userController.newUseRegister(ValidUser);
+        verify(userService).addUser(userCaptor.capture());
+
+        User addedUser = userCaptor.getValue();
+
+        // Then
+        assertEquals("ROLE_NORMAL", addedUser.getRole()); // Ensure role is set to ROLE_NORMAL
+        assertEquals("TestUser",addedUser.getUsername()); // Ensure role is set to ROLE_NORMAL
+        assertEquals("TestAddress",addedUser.getAddress());
+        assertEquals("Test@example.com",addedUser.getEmail());
+        assertEquals("password123",addedUser.getPassword());
+        assertEquals("redirect:/", result1); // Ensure redirection to home page
+        verify(userService).addUser(addedUser); // Ensure the user is added using the service
+    }
+}
