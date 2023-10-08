3609dd4db20c0172632a348306692ff35c0cfece
Nopparwut Sirisawat <nopparwut.sir@student.mahidol.edu>
2023-10-09 01:05:16 +0700

    delete product by id

---
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/DeleteProductTests.java
@@ -0,0 +1,37 @@
+package com.jtspringproject.JtSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.jtspringproject.JtSpringProject.models.Product;
+import com.jtspringproject.JtSpringProject.dao.productDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Profile("test")
+public class DeleteProductTests {
+
+    @Autowired
+    private productDao productService;
+
+    @Test
+    @Transactional
+    public void testDeleteProduct() {
+
+        Product product = new Product();
+        product.setName("ProductTest01");
+        product.setPrice(99);
+        productService.addProduct(product);
+
+        int productId = product.getId();
+        assertNotNull(productService.getProduct(productId));
+
+        boolean isDeleted = productService.deletProduct(productId);
+        assertTrue(isDeleted);
+
+        assertNull(productService.getProduct(productId));
+    }
+
+}
