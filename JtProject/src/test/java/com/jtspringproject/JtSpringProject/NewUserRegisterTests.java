8ef4e4ee457b1e206494d4e158009793a9ba6ad2
Kittipat Arpanon <kittipat.arp@student.mahidol.ac.th>
2023-10-10 13:05:05 +0700

    update test case to match requirement

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/NewUserRegisterTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/NewUserRegisterTests.java
@@ -3,6 +3,7 @@ package com.jtspringproject.JtSpringProject;
 import com.jtspringproject.JtSpringProject.models.User;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.jtspringproject.JtSpringProject.controller.UserController;
@@ -30,7 +31,9 @@ public class NewUserRegisterTests {
     }
     @Test
     public void testNewUserRegister1() {
-        //Given Valid register new user
+        //(False, Capable)
+        //'4', 'TestAddress', 'Test@example.com', 'password123', 'ROLE_NORMAL', 'TestUser'
+        //expected result (The method able to add new user to the table and return “redirect:/”)
         User ValidUser = new User();
         ValidUser.setUsername("TestUser");
         ValidUser.setAddress("TestAddress");
@@ -38,7 +41,7 @@ public class NewUserRegisterTests {
         ValidUser.setPassword("password123");
 
         // When
-        String result1 = userController.newUseRegister(ValidUser);
+        String result = userController.newUseRegister(ValidUser);
         verify(userService).addUser(userCaptor.capture());
 
         User addedUser = userCaptor.getValue();
@@ -49,7 +52,76 @@ public class NewUserRegisterTests {
         assertEquals("TestAddress",addedUser.getAddress());
         assertEquals("Test@example.com",addedUser.getEmail());
         assertEquals("password123",addedUser.getPassword());
-        assertEquals("redirect:/", result1); // Ensure redirection to home page
-        verify(userService).addUser(addedUser); // Ensure the user is added using the service
+        assertEquals("redirect:/", result); // Ensure redirection to home page
+        verify(userService, times(1)).addUser(ValidUser);
+    }
+    @Test
+    public void testNewUserRegister2(){
+        //(False, Incapable)
+        //'4', 'TestAddress', 'invalidemailformat', 'password123', 'ROLE_NORMAL', 'TestUser'
+        //expected result (The method unable to add new user to the table and return “redirect:/”)
+        User ValidUser = new User();
+        ValidUser.setUsername("TestUser");
+        ValidUser.setAddress("TestAddress");
+        ValidUser.setEmail("invalidemailformat");
+        ValidUser.setPassword("password123");
+        // When
+        String result = userController.newUseRegister(ValidUser);
+        verify(userService).addUser(userCaptor.capture());
+
+        User addedUser = userCaptor.getValue();
+        assertEquals("ROLE_NORMAL", addedUser.getRole()); // Ensure role is set to ROLE_NORMAL
+        assertEquals("TestUser",addedUser.getUsername()); // Ensure role is set to ROLE_NORMAL
+        assertEquals("TestAddress",addedUser.getAddress());
+        assertEquals("invalidemailformat",addedUser.getEmail());
+        assertEquals("password123",addedUser.getPassword());
+        assertEquals("redirect:/", result);
+        verify(userService, times(0)).addUser(ValidUser);
+    }
+    @Test
+    public void testNewUserRegister3(){
+        //(True,Capable)
+        //'4',null, 'Test@example.com', 'password123', 'ROLE_NORMAL', 'TestUser'
+        //expected result (The method able to add new user to the table and return “redirect:/”)
+        User ValidUser = new User();
+        ValidUser.setUsername("TestUser");
+        ValidUser.setAddress(null);
+        ValidUser.setEmail("Test@example.com");
+        ValidUser.setPassword("password123");
+        // When
+        String result = userController.newUseRegister(ValidUser);
+        verify(userService).addUser(userCaptor.capture());
+
+        User addedUser = userCaptor.getValue();
+        assertEquals("ROLE_NORMAL", addedUser.getRole()); // Ensure role is set to ROLE_NORMAL
+        assertEquals("TestUser",addedUser.getUsername()); // Ensure role is set to ROLE_NORMAL
+        assertNull(addedUser.getAddress());
+        assertEquals("Test@example.com",addedUser.getEmail());
+        assertEquals("password123",addedUser.getPassword());
+        assertEquals("redirect:/", result);
+        verify(userService, times(1)).addUser(ValidUser);
+    }
+    @Test
+    public void testNewUserRegister4(){
+        //(True, Incapable)
+        //'4',null, 'Test@example.com', 'password123', 'ROLE_NORMAL', 'TestUser'
+        //expected result (The method unable to add new user to the table and return “redirect:/”)
+        User ValidUser = new User();
+        ValidUser.setUsername(null);
+        ValidUser.setAddress(null);
+        ValidUser.setEmail("Test@example.com");
+        ValidUser.setPassword("password123");
+        // When
+        String result = userController.newUseRegister(ValidUser);
+        verify(userService).addUser(userCaptor.capture());
+
+        User addedUser = userCaptor.getValue();
+        assertEquals("ROLE_NORMAL", addedUser.getRole()); // Ensure role is set to ROLE_NORMAL
+        assertNull(addedUser.getUsername()); // Ensure role is set to ROLE_NORMAL
+        assertNull(addedUser.getAddress());
+        assertEquals("Test@example.com",addedUser.getEmail());
+        assertEquals("password123",addedUser.getPassword());
+        assertEquals("redirect:/", result);
+        verify(userService, times(0)).addUser(ValidUser);
     }
 }
